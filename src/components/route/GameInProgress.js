9328c13a6b7dda4d23ef70429631a2af39d476cb
Jeremy Ward <jeremy.ward@webdevstudios.com>
2018-06-22 12:28:54 -0500

    Add React Router to project and update lock file.

--- src/components/route/GameInProgress.js
+++ src/components/route/GameInProgress.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import ScoreUpdater from "./ScoreUpdater";
-import PlayersList from "./PlayersList";
+import ScoreUpdater from "../ScoreUpdater";
+import PlayersList from "../PlayersList";
 
 class Game extends Component {
   render() {
---
+++ src/components/route/GameSetup.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+class GameSetup extends Component {
+  render() {
+    return <p>Please enter a name for each player.</p>;
+  }
+}
