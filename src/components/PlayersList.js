342c646f09158509b3ec97178d67944110908f99
Jeremy Ward <jeremy.ward@webdevstudios.com>
2018-06-24 13:22:31 -0500

    Render initial player score state.

--- src/components/PlayersList.js
+++ src/components/PlayersList.js
@@ -2,8 +2,24 @@ import React, { Component } from "react";
 import Player from "./Player";
 
 class PlayersList extends Component {
+  renderPlayers(players) {
+    let items = [];
+
+    for (let i = 0, playerCount = players.length; i < playerCount; i++) {
+      items.push(
+        <Player
+          key={i}
+          name={this.props.players[i].name}
+          score={this.props.players[i].score}
+        />
+      );
+    }
+
+    return items;
+  }
+
   render() {
-    return <Player name={this.props.name} score={0} />;
+    return this.renderPlayers(this.props.players);
   }
 }
 
