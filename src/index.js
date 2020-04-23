f43d6bf85bb40664c91af05f761a21fc755aa777
Jeremy Ward <jeremy@jmichaelward.com>
2020-04-23 13:53:56 -0500

    Rename reducers.js to store.js and move store creation logic into that file.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import {render} from "react-dom";
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import rootReducer from './reducers';
+import store from './store';
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-
-const store = createStore(rootReducer);
 
 render(
     <Provider store={store}>
