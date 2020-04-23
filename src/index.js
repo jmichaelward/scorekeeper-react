ade4ba6d3e6f8090822f239b899b6b66bf82b475
Jeremy Ward <jeremy@jmichaelward.com>
2020-04-23 13:42:17 -0500

    Integrate initial redux scaffolding into app.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import {render} from "react-dom";
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import rootReducer from './reducers';
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("scorekeeper"));
+const store = createStore(rootReducer);
+
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById("scorekeeper")
+);
 registerServiceWorker();
