53c7cec0601c8e96f6ced02d35904671c0d82178
Jeremy Ward <jeremy@jmichaelward.com>
2021-05-01 09:00:46 -0500

    Replace the PlayersForm class-based component with functional.

--- src/components/PlayersForm/index.js
+++ src/components/PlayersForm/index.js
@@ -1,42 +1,53 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PlayerInput from "../PlayerInput";
 import Button from "../Button";
 import './PlayersForm.scss';
 
-class PlayersForm extends Component {
-  savePlayerData = event => {
+/**
+ * Component for the form to enter the list of players.
+ *
+ * @param props
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const PlayersForm = (props) => {
+  const [players, setPlayers] = useState(props.players);
+
+  /**
+   * Returns a set of player inputs for the form.
+   *
+   * @param players
+   * @param updateName
+   * @returns {*}
+   */
+  const getPlayerInputs = (players, updateName) => {
+    return players.map((player, index) => (
+      <div className="players-form__player" key={index}>
+        <p>Player {index + 1} name:</p>
+        <PlayerInput id={index} name={player.name} updateName={updateName} />
+      </div>
+    ));
+  };
+
+  const savePlayerData = event => {
     event.preventDefault();
-    this.props.setGameInitialized(true);
-    this.props.setupPlayerData(this.state.players);
+    props.setGameInitialized(true);
+    props.setupPlayerData(players);
   };
 
-  updateName = (index, name) => {
-    const players = this.props.players;
-
+  const updateName = (index, name) => {
     players[index].name = name;
-
-    this.setState({ players });
+    setPlayers(players);
   };
 
-  render() {
-    return (
-      <form className="players-form" onSubmit={this.savePlayerData}>
-        <div className="players-form">
-          {getPlayerInputs(this.props.players, this.updateName)}
-        </div>
-        <Button type="submit" label="Start game" wrapClass="game-start-button-wrap" />
-      </form>
-    );
-  }
+  return (
+    <form className="players-form" onSubmit={savePlayerData}>
+      <div className="players-form">
+        {getPlayerInputs(players, updateName)}
+      </div>
+      <Button type="submit" label="Start game" wrapClass="game-start-button-wrap" />
+    </form>
+  );
 }
-
-const getPlayerInputs = (players, updateName) => {
-  return players.map((player, index) => (
-    <div className="players-form__player" key={index}>
-      <p>Player {index + 1} name:</p>
-      <PlayerInput id={index} name={player.name} updateName={updateName} />
-    </div>
-  ));
-};
 
 export default PlayersForm;
