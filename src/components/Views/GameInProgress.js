ca49c10bf1432e6a7587b61f56f9e141f3117d7d
Jeremy Ward <jeremy@jmichaelward.com>
2021-05-01 14:55:34 -0500

    Format GameInProgress.

--- src/components/Views/GameInProgress.js
+++ src/components/Views/GameInProgress.js
@@ -1,67 +1,67 @@
-import React, {useState} from "react";
-import PlayersList from "../PlayersList";
-import {saveGameState} from "../../App";
-import ScoreAdjustmentForm from "../ScoreAdjustmentForm";
+import React, { useState } from 'react'
+import PlayersList from '../PlayersList'
+import { saveGameState } from '../../App'
+import ScoreAdjustmentForm from '../ScoreAdjustmentForm'
 
 const GameInProgress = props => {
-    const [activePlayer, setActivePlayer] = useState(props.activePlayer);
-    const [playerCount] = useState(props.playerCount);
-    const [players] = useState(props.players);
+  const [activePlayer, setActivePlayer] = useState(props.activePlayer)
+  const [playerCount] = useState(props.playerCount)
+  const [players] = useState(props.players)
 
-    const getPlayerId = event => {
-        const player = event.target.closest('.player');
+  const getPlayerId = event => {
+    const player = event.target.closest('.player')
 
-        return null === player ? activePlayer : parseInt(player.getAttribute('data-player'), 10);
-    };
+    return null === player ? activePlayer : parseInt(player.getAttribute('data-player'), 10)
+  }
 
-    const determineActivePlayer = event => {
-        setActivePlayer(getPlayerId(event));
+  const determineActivePlayer = event => {
+    setActivePlayer(getPlayerId(event))
 
-        saveGameState({
-            players,
-            playerCount,
-            activePlayer
-        });
-    };
+    saveGameState({
+      players,
+      playerCount,
+      activePlayer
+    })
+  }
 
-    const handleScoreUpdate = event => {
-        event.preventDefault();
+  const handleScoreUpdate = event => {
+    event.preventDefault()
 
-        const form = event.target;
-        const score = parseInt(form.querySelector("#scoreInput").value, 10);
+    const form = event.target
+    const score = parseInt(form.querySelector('#scoreInput').value, 10)
 
-        setPlayerScore(activePlayer, score);
-        setNextActivePlayer();
+    setPlayerScore(activePlayer, score)
+    setNextActivePlayer()
 
-        form.reset();
-    };
+    form.reset()
+  }
 
-    const setPlayerScore = (player, score) => {
-        players[activePlayer].score += isNaN(score) ? 0 : score;
-    };
+  const setPlayerScore = (player, score) => {
+    players[ activePlayer ].score += isNaN(score) ? 0 : score
+  }
 
-    const setNextActivePlayer = () => {
-        const nextActivePlayer = activePlayer < ( props.playerCount - 1 ) ? activePlayer + 1 : 0;
-        setActivePlayer(nextActivePlayer);
+  const setNextActivePlayer = () => {
+    const nextActivePlayer = activePlayer < ( props.playerCount - 1 ) ? activePlayer + 1 : 0
+    setActivePlayer(nextActivePlayer)
 
-        saveGameState({
-            players,
-            playerCount,
-            activePlayer
-        });
-    };
+    saveGameState({
+      players,
+      playerCount,
+      activePlayer
+    })
+  }
 
-    return (
-        <div className="scorekeeper-game" onClick={(event) => determineActivePlayer(event)}>
-            <h2>Update Player Score</h2>
-            <ScoreAdjustmentForm handler={handleScoreUpdate}/>
-            <PlayersList
-                id="players-list"
-                players={players}
-                activePlayer={activePlayer}
-            />
-        </div>
-    );
-};
+  return (
+    <div className="scorekeeper-game" onClick={determineActivePlayer}>
+      <h2>Update Player Score</h2>
+      <ScoreAdjustmentForm handler={handleScoreUpdate}/>
+      <PlayersList
+        id="players-list"
+        players={players}
+        activePlayer={activePlayer}
+      />
+    </div>
+  )
+}
 
-export default GameInProgress;
+export default GameInProgress
